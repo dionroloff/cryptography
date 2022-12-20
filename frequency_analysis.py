4bb336bb5415bc64ce9efa201d49ea0264d659f8
Dion Roloff <dionroloff@protonmail.com>
2022-12-19 18:35:38 -0800

    refactor

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,18 +1,31 @@
 cipher = input("Type or paste some text to analyze its letter frequency:").upper()
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-freq = {}
-for c in alphabet:
-    freq[c] = 0
+class Attack:
+    def __init__(self):
+        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.freq = {}
 
-letter_count = 0
-for c in cipher:
-    if c in freq:
-        freq[c] += 1
-        letter_count += 1
+    def calculate_freq(self, cipher):
+        for c in self.alphabet:
+            self.freq[c] = 0
 
-for c in freq:
-    freq[c] = round(freq[c]/letter_count, 4)
+        letter_count = 0
+        for c in cipher:
+            if c in self.freq:
+                self.freq[c] += 1
+                letter_count += 1
 
-for c in freq:
-    print(c, ":", freq[c])
+        for c in self.freq:
+            self.freq[c] = round(self.freq[c]/letter_count, 4)
+
+    def print_freq(self):
+        new_line_count = 0
+        for c in self.freq:
+            print(c, ":", self.freq[c], ' ', end='')
+            if new_line_count % 3 == 2:
+                print()
+            new_line_count += 1
+
+attack = Attack()
+attack.calculate_freq(cipher)
+attack.print_freq()
