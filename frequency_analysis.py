d8d05b82239785a47c734386f47b9966b89e263e
Dion Roloff <dionroloff@protonmail.com>
2023-01-02 15:14:35 -0800

    add guess key

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,10 +1,12 @@
 import operator
 
-cipher = input("Type or paste some text to analyze its letter frequency:").upper()
+cipher = input("Type or paste some text to analyze its letter frequency:").lower()
 
 class Attack:
     def __init__(self):
-        self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.plain_chars_left = "abcdefghijklmnopqrstuvwxyz"
+        self.cipher_chars_left = "abcdefghijklmnopqrstuvwxyz"
         self.freq = {}
         self.freq_eng = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223,
                'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
@@ -41,11 +43,33 @@ class Attack:
                 map[plain_char] = round(abs(self.freq[cipher_char] - self.freq_eng[plain_char]), 4)
             self.mappings[cipher_char] = sorted(map.items(), key=operator.itemgetter(1))
 
+    def guess_key(self):
+        key = {}
+        for cipher_char in self.cipher_chars_left:
+            for plain_char, diff in self.mappings[cipher_char]:
+                if plain_char in self.plain_chars_left:
+                    key[cipher_char] = plain_char
+                    self.plain_chars_left = self.plain_chars_left.replace(plain_char, '')
+                    break
+        return key
+
+def decrypt(key, cipher):
+    message = ""
+    for c in cipher:
+        if c in key:
+            message += key[c]
+        else:
+            message += c
+    return message
+
+
 attack = Attack()
 attack.calculate_freq(cipher)
 attack.print_freq()
 attack.calculate_matches()
+key = attack.guess_key()
 
 print()
-for c in attack.mappings:
-    print(c, attack.mappings[c])
+print(key)
+message = decrypt(key, cipher)
+print(message)
