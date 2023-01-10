f5d4da1a8fe35818f3286deb1136bfae8e4e3cff
Dion Roloff <dionroloff@protonmail.com>
2023-01-09 17:37:29 -0800

    otp changes

--- one_time_pad.py
+++ one_time_pad.py
@@ -1,6 +1,6 @@
 import random
 
-# generate random key string and return in as binary
+# generate random key string and return it as binary
 def generate_key_stream(n):
     return bytes([random.randrange(0, 256) for i in range(n)])
 
@@ -9,9 +9,11 @@ def xor_bytes(key_stream, message):
     length = min(len(key_stream), len(message))
     return bytes([key_stream[i] ^ message[i] for i in range(length)])
 
-message = input("Type a message to be encrypted: ").encode()
+message = "MESSAGE1".encode()
 key_stream = generate_key_stream(len(message))
 cipher = xor_bytes(key_stream, message)
-print("Key stream: ", key_stream)
+
 print("Cipher: ", cipher)
-print(xor_bytes(key_stream, cipher))
+message = "MESSAGE2".encode()
+guess_key_stream = xor_bytes(message, cipher)
+print(xor_bytes(guess_key_stream, cipher))
