c6116158adf91a131b42a4e12567e0bb74f75f04
Dion Roloff <dionroloff@protonmail.com>
2023-01-15 13:38:10 -0800

    modify function

--- des.py
+++ des.py
@@ -1,9 +1,15 @@
 from pyDes import *
 
-message = "0123456701234567"
+def modify(cipher):
+    mod = [0] * len(cipher)
+    mod[8] = 1
+    return bytes([mod[i] ^ cipher[i] for i in range(len(cipher))])
+
+message = "This is a message."
 key = "DECRYPTS"
 iv = bytes([0] * 8)
-k = des(key, ECB, iv, pad=None, padmode=PAD_PKCS5)
+# CBC mode makes the message XOR'd with IV before encryption
+k = des(key, CBC, iv, pad=None, padmode=PAD_PKCS5)
 cipher = k.encrypt(message)
 
 print("Length of plaintext: ", len(message))
@@ -11,5 +17,9 @@ print("Length of ciphertext: ", len(cipher))
 print("Encrypted: ", cipher[0:8])
 print("Encrypted: ", cipher[8:16])
 print("Encrypted: ", cipher[16:])
+
+cipher = modify(cipher)
+print("Modified cipher: ", cipher)
+
 messge = k.decrypt(cipher)
 print("Decrypted: ", message)
